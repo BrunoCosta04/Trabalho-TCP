7a2e9d0f5f4a401de3ef62deb1987e9d536e84cc
Marcus Farias <marcusczupriniakifarias@gmail.com>
2024-02-07 19:35:01 -0300

    limpando main e colocando input validation valendo

--- MusicPlayer/MusicPlayer/MusicPlayer.cs
+++ MusicPlayer/MusicPlayer/MusicPlayer.cs
@@ -52,9 +52,7 @@ namespace MusicPlayer
         {
             try
             {
-                string path = Directory.GetCurrentDirectory();
 
-                FileService.Download(Music.Song, path);
             }
             catch (Exception ex)
             {
@@ -63,69 +61,29 @@ namespace MusicPlayer
         }
         private void btnTocarMusica_Click(object sender, EventArgs e)
         {
-            this.TocarMusica3();
+            try
+            {
 
-            //MidiEventCollection midiEvents = new MidiEventCollection(0, 480);
-
-            //MusicGeneratorService.AddEventsFromText(midiEvents, 60);
-            //MusicGeneratorService.AddEventsFromText(midiEvents, 65);
-            //MusicGeneratorService.AddEventsFromText(midiEvents, 70);
-            //MusicGeneratorService.AddEventsFromText(midiEvents, 75);
-
-            //MidiOut midiOut = new MidiOut(0);
-            //foreach (var midiEvent in midiEvents[0])
-            //{
-            //    midiOut.Send(midiEvent.GetAsShortMessage());
-            //}
-            //midiOut.Close();
-
-
-            //try
-            //{
-            //    string music = txtMusic.Text;
-            //    int octaveValue = Convert.ToInt32(cmbOctaves.SelectedValue);
-            //    int instrumentValue = Convert.ToInt32(cmbInstruments.SelectedValue);
-            //    int volume = tbVolume.Value;
-
-            //    bool isAbleToPlay = InputValidationService.UserInputValidation(txtMusic.Text, octaveValue, instrumentValue);
-
-            //    if (isAbleToPlay)
-            //    {
-            //        //generate music
-
-            //        //play Music
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(ex.Message);
-            //}
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
         private void btnGenerateMusic_Click(object sender, EventArgs e)
         {
             try
             {
-                MidiEventCollection midiEvents = new MidiEventCollection(0, 480);
+                string music = txtMusic.Text;
+                var selectedIntrument = Convert.ToInt32(cmbInstruments.SelectedValue);
+                var selectedOctave = Convert.ToInt32(cmbOctaves.SelectedValue);
 
-                NoteEvent noteOn = new NoteOnEvent(2000, 1, 60, 127, 0);
-                NoteEvent noteOff = new NoteEvent(2000, 1, MidiCommandCode.NoteOff, 60, 0);
+                bool isAbleToGenerateMusic = InputValidationService.UserInputValidation(music, selectedIntrument, selectedOctave);
 
-                midiEvents.AddEvent(noteOn, 0);
-                midiEvents.AddEvent(noteOff, 0);
+                if (isAbleToGenerateMusic)
+                {
 
-                noteOn = new NoteOnEvent(1000, 1, 45, 127, 0);
-                noteOff = new NoteEvent(1000, 1, MidiCommandCode.NoteOff, 45, 0);
-
-                midiEvents.AddEvent(noteOn, 0);
-                midiEvents.AddEvent(noteOff, 0);
-
-                this.Music.ChangeMusic(midiEvents);
-
-
-
-
-
-
+                }
             }
             catch (Exception ex)
             {
@@ -143,7 +101,6 @@ namespace MusicPlayer
             var data = DataBase.Instruments;
 
             cmbInstruments.DataSource = data.BindComboBox(x => x.Name, x => x.Id, true);
-            //MessageBox.Show(cmbInstruments.);
         }
 
         private void LoadComboxOctaves()
@@ -159,96 +116,6 @@ namespace MusicPlayer
         private void LoadMusic()
         {
             this.Music = new Music();
-        }
-        private void TocarMusica2()
-        {
-            int numberDevice = 0;
-            int channel = 1;
-            int noteNumber = 50;
-
-
-            using (var midiOut = new MidiOut(numberDevice))
-            {
-                var noteOnEvent = new NoteOnEvent(0, channel, noteNumber, 100, 50);
-                midiOut.Send(noteOnEvent.GetAsShortMessage());
-            }
-
-            //TempoEvent tempoEvent = new TempoEvent();
-
-
-
-
-
-            string instrumento;
-
-        }
-
-        private void TocarMusica1()
-        {
-            //MidiEventCollection midiEvents = new MidiEventCollection(0, 1000);
-
-            //NoteEvent noteEvent = new NoteOnEvent(0, 1, 60, 127, 5000);
-            //midiEvents.AddEvent(noteEvent, 0);
-            //noteEvent = new NoteOnEvent(1000, 1, 45, 127, 10);
-            //midiEvents.AddEvent(noteEvent, 0);
-
-            //MidiOut midiOut = new MidiOut(0);
-
-
-            //var teste2 = midiEvents[0][0];
-
-            //midiOut.Send(midiEvents[0][0].GetAsShortMessage());
-            //midiOut.Send(midiEvents[0][1].GetAsShortMessage());
-
-            //TempoEvent tempoEvent = new TempoEvent();
-        }
-
-        private void TocarMusica3()
-        {
-            var song = this.Music.Song;
-
-            using (var midiOut = new MidiOut(0))
-            {
-                var note = song[0][0];
-
-                midiOut.Send(note.GetAsShortMessage());
-                Thread.Sleep((int)note.AbsoluteTime);
-                note = song[0][1];
-                midiOut.Send(note.GetAsShortMessage());
-
-                note = song[0][2];
-                midiOut.Send(note.GetAsShortMessage());
-                Thread.Sleep((int)note.AbsoluteTime);
-                note = song[0][3];
-                midiOut.Send(note.GetAsShortMessage());
-            }
-
-            string path = "C:\\Users\\Marcus\\Desktop\\teste.mid";
-            FileService.Download(song, path);
-
-
-            using (var midiOut = new MidiOut(0))
-            {
-                MidiFile midiFile = new MidiFile(path);
-
-                foreach (var teste in midiFile.Events[0])
-                {
-                    midiOut.Send(teste.GetAsShortMessage());
-
-
-
-
-                    Thread.Sleep((int)teste.AbsoluteTime);
-                }
-            }
-
-
-
-
-
-
-
-
         }
         #endregion Methods
     }
--- MusicPlayer/MusicPlayer/Services/InputValidationService.cs
+++ MusicPlayer/MusicPlayer/Services/InputValidationService.cs
@@ -8,67 +8,62 @@ namespace MusicPlayer.Services
 {
     internal static class InputValidationService
     {
-        //Todos os caracteres são validos. Isso pode ser útil para o GUI.
-        private static readonly char[] validChars = { 'A', 'B', 'C', 'D', 'E', 'F', 'G','H','I',
-                                                      'J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y',
-                                                      'Z','?',';','+', '-' };
         public static bool UserInputValidation(string music, int instrumentSelectedValue, int octaveSelectedValue)
         {
             bool sucesso = true;
-
+            int number = 0;
             StringBuilder sb = new StringBuilder();
 
             if (instrumentSelectedValue < 0)
             {
+                number++;
                 sb.AppendLine("Necessário escolher um instrumento!");
                 sucesso = false;
             }
-                
 
             if (octaveSelectedValue < 0)
             {
+                number++;
                 sb.AppendLine("Necessário escolher uma oitava!");
                 sucesso = false;
             }
-                
 
             if (!MusicCharValidator(music))
             {
-                sb.AppendLine("Existem caracteres inválidos no texto!");
+                number++;
+                sb.AppendLine("O texto está vazio ou contém caracteres inválidos!");
                 sucesso = false;
             }
 
-            if (!sucesso) 
-            {   
+            if (!sucesso)
+            {
+                sb.Insert(0, $"Existem {number} erro(s) em seu pedido: \n");
                 string errorMessage = sb.ToString();
-                throw new Exception(errorMessage);
+                MessageBox.Show(errorMessage);
             }
-
-
 
             return sucesso;
         }
+
         //Valida se o texto excrito ou em texto não contem nenhum caractere desconhecido, que não esta na tabela ASCII.
-       
         private static bool MusicCharValidator(string music)
         {
-            bool sucesso = true;
-            int letterValue = 0;
+            bool success = true;
+
+            int letterValue;
+            if (string.IsNullOrEmpty(music))
+                return false;
+
             foreach (char letter in music)
             {
-                letterValue = (int)letter; 
-             //deve retornar uma mensagem de 'caractere inválido'
+                letterValue = (int)letter;
+
                 if (!(letterValue >= 0 && letterValue <= 127))
-                {   
-                    sucesso = false;
-                    break;
-                }
+                    return false;
             }
 
-
-
-
-            return sucesso;
+            return success;
         }
     }
 }
+
